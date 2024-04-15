b96d1bf3cedd89840e025c371e531febf27d4ec7
akram <im_noui@esi.dz>
2024-04-15 03:58:29 +0200

    Revert "Merge branch 'master' into beta"
    This reverts commit 548a1adbb34f4ec81ec5bf4ad41523571ebc781f, reversing
    changes made to 97fb42598bde1d535ed4a67ee92daa39d1f13918.

--- app.js
+++ app.js
@@ -19,16 +19,15 @@ const establishmentsRouter = require('./routes/establishments.router');
 const ordersRouter = require('./routes/orders.router');
 const User = require('./models/users.model.js')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Initialize express app
+const app = express();
 
-var app = express();
-
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 
 // Security Middleware
 app.use(helmet());
